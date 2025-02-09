a93e128f26e40505d79735c8afc695c438cec335
Leon van Kammen <leonvankammen@gmail.com>
2025-02-09 12:54:37 +0100

    updated aframe to last version to mitigate stencil rendering

--- dist/xrfragment.aframe.js
+++ dist/xrfragment.aframe.js
@@ -1,4 +1,9 @@
 /*
+ * v0.5.1 generated at Sun Feb  9 12:52:39 PM CET 2025
+ * https://xrfragment.org
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+/*
  * v0.5.1 generated at Wed Jan 15 10:52:05 AM CET 2025
  * https://xrfragment.org
  * SPDX-License-Identifier: AGPL-3.0-or-later
